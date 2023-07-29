3123b27ebabce571385b6114dce7744de442c824
一个 名称 <wojiaoappl_0128@qq.com>
2023-07-29 13:41:37 +0800

    New class code

--- TEST.py
+++ TEST.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import os
+
+class GUI:
+    def __init__(self):
+        print("No")
+
+    def exits(self):
+        print("No2")
+
+if __name__ == "__main__":
+    GUI().exits()
